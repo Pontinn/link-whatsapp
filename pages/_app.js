13fda008245de2f37618eb198e8353c338f9cded
Leonardo Pontin <leo.pontin2@gmail.com>
2025-01-07 09:57:40 -0300

    Adicionado: Titulo e descrição para site

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+// Importação do CSS do elemento de input de telefone
 import "intl-tel-input/build/css/intlTelInput.css";
+// Importação do Head do Next.js
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Gerador de links para Whatsapp</title>
+        <meta
+          name="description"
+          content="Crie links personalizados para WhatsApp em segundos e facilite sua comunicação. Seja para vendas, suporte ou networking, ofereça uma conexão direta e eficiente com seus clientes. Experimente agora e veja como é simples!"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
