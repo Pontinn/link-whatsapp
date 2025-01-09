dbf4891fe51fb6a3a5978a4395ab4aa77c8f117c
Leonardo Pontin <leo.pontin2@gmail.com>
2025-01-09 11:10:42 -0300

    Correção anuncio

--- pages/link-gerado.js
+++ pages/link-gerado.js
@@ -48,12 +48,10 @@ const LinkGerado = () => {
             display: "block",
             width: "100%",
             height: "280px",
-            backgroundColor: "#f2f2f2",
           }}
           data-ad-client="ca-pub-3374247426589530"
           data-ad-slot="8359202329"
           data-ad-format="auto"
-          data-adtest="on"
           data-full-width-responsive="true"
         ></ins>
       </section>
